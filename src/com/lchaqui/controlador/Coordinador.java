aebcb8724aea1e28f033a724e3f920421dc2acee
LuisChaqui <luigi92pm@gmail.com>
2018-11-13 19:41:39 -0500

    Proyecto Inicial Coordenadas Geograficas

---
+++ src/com/lchaqui/controlador/Coordinador.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lchaqui.controlador;
+
+import com.lchaqui.modelo.Punto;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author HP
+ */
+public class Coordinador {
+    ArrayList<Punto> puntos= new ArrayList<Punto>();
+   
+    
+    public void almacenarPuntos(Punto p){
+        p.setX(Integer.parseInt(JOptionPane.showInputDialog("Ingresa X:")));
+         p.setY(Integer.parseInt(JOptionPane.showInputDialog("Ingresa Y:")));
+         puntos.add(p);
+    }
+    public void calcularDistanciaEntreDosPuntos(){
+        int p1,p2,cont=0;
+        //do{
+        p1 = Integer.parseInt(JOptionPane.showInputDialog(imprimirPuntos()+"\nIngresa un punto"))-1;
+        p2 = Integer.parseInt(JOptionPane.showInputDialog(imprimirPuntos()+"\nIngresa un punto"))-1;
+        double v1=Math.pow((puntos.get(p2).getX())-(puntos.get(p1).getX()),2);
+        double v2=Math.pow((puntos.get(p2).getY())-(puntos.get(p1).getY()),2);
+        JOptionPane.showMessageDialog(null,""+Math.sqrt(v1+v2));
+//        cont++;
+        //}while(cont!=2);
+//imprimirPuntos();
+    }
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     public String imprimirPuntos(){
+        String datos="";
+        for (Punto punto : puntos) {
+            datos +="\n"+(puntos.indexOf(punto)+1)+".- "+punto.toString();
+        }
+        //JOptionPane.showMessageDialog(null, datos);
+        return datos;
+    }
+     
+     public void menu(){
+        int op=0;
+        
+        do{
+        try{
+            op = Integer.parseInt(JOptionPane.showInputDialog("Seleccione una opcion:\n"
+                 + "1.- Ingresar Punto\n"
+                 + "2.- Calcular Distancia entre dos puntos\n"
+                 + "3.- Calcular distancia a la torre de control\n"
+                 + "4.- Imprimir Puntos\n"
+                 + "5.- Salir"));
+            switch (op) {
+                case 1:
+                    almacenarPuntos(new Punto());
+                    break;
+                case 2:
+                    calcularDistanciaEntreDosPuntos();
+                    System.out.println("CAso 2");
+                    break;
+                case 3:
+                    
+                    break; 
+                case 4:
+                    JOptionPane.showMessageDialog(null,imprimirPuntos());
+                    break;
+                
+            } 
+        }catch(Exception e){
+            System.out.println("error");
+            
+        }
+           
+            
+        }while(op!=6);
+                 
+     }
+    
+}
+
--- src/com/lchaqui/vista/Aplicacion.java
+++ src/com/lchaqui/vista/Aplicacion.java
@@ -5,6 +5,7 @@
  */
 package com.lchaqui.vista;
 
+import com.lchaqui.controlador.Coordinador;
 import com.lchaqui.modelo.Punto;
 import javax.swing.JOptionPane;
 
@@ -14,35 +15,8 @@ import javax.swing.JOptionPane;
  */
 public class Aplicacion {
     public static void main(String[] args) {
-        Punto p1 = new Punto(4, 3);
-        Punto p2 = new Punto(10, 15);
-        int op;
-        do {            
-         op=Integer.parseInt(JOptionPane.showInputDialog("Deseas ingresar un punto: \n"
-            +"1.- Si \n"
-            +"2.- No" ));
-            if (op==1) {
-                int x= Integer.parseInt(JOptionPane.showInputDialog("Ingrese coordenadas X: "));
-                int y=Integer.parseInt(JOptionPane.showInputDialog("Ingrese coordenadas Y: "));
-             Punto.almacenarPuntos(x, y);  
-             
-            }
-  //
-         
-        } while (op!=2);
-        Punto.imprimirPuntos();
-//        int op1;
-//        do {            
-//            op1=Integer.parseInt(JOptionPane.showInputDialog("Deseas calcular la distancia de dos puntos un punto: \n"
-//            +"1.- Si \n"
-//            +"2.- No" ));
-//            if (op1==1) {
-//            JOptionPane.showMessageDialog(null,p1.toString()+"\n"+ p2.toString()+"\n"+ "La distancia entre los punto son: "+ Punto.CalcularDistancia(p1, p2));
-////Punto.CalcularDistancia(p1, p2);
-//            }
-//        } while (op1!=2);
-        
-       
-        
+         Coordinador coordinador= new Coordinador();
+         coordinador.menu();
     }
 }
+   
